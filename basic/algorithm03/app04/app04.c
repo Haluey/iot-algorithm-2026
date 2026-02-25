4ea45a72876dffe2e07bb48e345a359e34fe5be4
Haluey <cheong7394wol@gmail.com>
2026-02-25 14:11:18 +0900

    자료구조 끝!

--- basic/algorithm03/app04/app04.c
+++ basic/algorithm03/app04/app04.c
@@ -1,11 +1,18 @@
 ﻿// p178. 재귀 알고리즘 분석
+// 하향식 방법 분석
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
+// 함수호출횟수 카운터 변수
+int callCnt = 0;
+
 /*--- 재귀함수 recur 선언과 정의 ---*/
 void recur(int n) {
+	
+	callCnt++;	// 함수 호출횟수 증가
+
 	if (n > 0) {	// 종료 조건 n이 0보다 크다. 0이하로 떨어지면 종료된다
 		recur(n - 1);		// 1. 1 - 1이 될 때까지 3번 반복
 		printf("%d\n", n);	// 2. 1번 반복이 끝나야 출력이 시작
@@ -21,7 +28,9 @@ int main(void) {
 		
 		if (x <= 0) break;
 
+		callCnt = 0;	// 입력받을 때마다 초기화
 		recur(x);
+		printf("함수 총 호출횟수 : %d\n\n", callCnt);
 	}
 
 	return 0;
